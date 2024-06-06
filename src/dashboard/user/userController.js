e960e7d54d6e0924121bce185ddf66071e43d026
TrippyRahul <TrippyRahul@users.noreply.github.com>
2024-06-06 10:50:13 +0000

    added: build artifacts

--- src/dashboard/user/userController.js
+++ src/dashboard/user/userController.js
@@ -94,7 +94,7 @@ const loginUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             // secure: true,
             sameSite: "none",
         });
-        return res.status(200).json({ message: "Login successful" });
+        return res.status(200).json({ message: "Login successful", token: token });
     }
     catch (err) {
         return res
