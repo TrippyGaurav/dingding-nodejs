62b5e2806d681943dd0c257a387820f661056dfa
TrippyRahul <rahulrawat.trippybug@gmail.com>
2024-06-13 13:05:25 +0530

    Add:favourite games api

--- src/dashboard/casinoGames/gamesRoutes.ts
+++ src/dashboard/casinoGames/gamesRoutes.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import { verifyToken } from "../../middleware/tokenAuth";
-import { getGames, sendGames, changeGames, image } from "./gamesController";
+import {
+  getGames,
+  sendGames,
+  changeGames,
+  image,
+  favourite,
+} from "./gamesController";
 import { verifyAuth } from "../../middleware/auth";
 const Games = express.Router();
 //POST ROUTES
 Games.post("/add", verifyToken, sendGames);
 //GET ROUTES
-Games.get("/getGames", verifyAuth, getGames);
+Games.get("/getGames", verifyToken, getGames);
+Games.post("/upload", image);
+Games.post("/favourite", verifyToken, favourite);
 //PUT REQUEST
 Games.put("/changes", verifyAuth, changeGames);
-Games.post("/upload", image);
 export default Games;
