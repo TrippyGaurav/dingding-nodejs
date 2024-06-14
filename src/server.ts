bcc26ab1fb27397f3c72ff4610e09686cf28b863
TrippyRahul <129256734+TrippyRahul@users.noreply.github.com>
2024-06-14 11:04:10 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,18 @@ const app = express();
 //Cloudinary configs
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
+const allowedOrigins = ['http://localhost:3000', 'https://game-rtp-backend-w7g7.onrender.com'];
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
 //cors config
 const corsOptions = {
   origin: [
