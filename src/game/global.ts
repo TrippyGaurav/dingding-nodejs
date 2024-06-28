7294d25660cafa4b3cd74f7688c4f949512f2ddb
TrippyRahul <rahulrawat.trippybug@gmail.com>
2024-06-28 16:55:47 +0530

    added SL-PM

--- src/game/global.ts
+++ src/game/global.ts
@@ -47,6 +47,7 @@ export const gameSettings: GameSettings = {
   fullPayTable: [],
   _winData: undefined,
   freeSpinStarted : false,
+  resultReelIndex: [],
   jackpot: {
     symbolName: "",
     symbolsCount: 0,
@@ -78,7 +79,7 @@ export const gameSettings: GameSettings = {
     gameSettings.Weights = [];
     gameSettings._winData = new WinData(clientID);
 
-    gameSettings.currentGamedata = GameData[0] ;
+    gameSettings.currentGamedata = GameData[0] ||GameData ;
     // gameSettings.currentBet = 
     gameSettings.currentGamedata.Symbols.forEach((element) => {
       if (element.Name == "Bonus") {
@@ -278,59 +279,101 @@ export function startFreeSpin()
 }
 export function checkforMoolah() {
   console.log("_______-------CALLED FOR CHECK FOR MOOLAHHHH-------_______");
-  
+
+
   gameSettings.tempReels = gameSettings.reels;
   const lastWinData = gameSettings._winData;
   lastWinData.winningSymbols = combineUniqueSymbols(
     removeRecurringIndexSymbols(lastWinData.winningSymbols)
   );
   const index = lastWinData.winningSymbols.map((str) => {
-    const index : {x, y} = str.split(",").map(Number);
+    const index: { x, y } = str.split(",").map(Number);
     return index;
   });
   console.log("Winning Indexes " + index);
-
+  let matrix = [];
+  matrix = gameSettings.resultSymbolMatrix;
   index.forEach(element => {
-    console.log("X : " + element[0] + " Y : " + element[1]);
-    console.log("REEL LENGTH " +gameSettings.tempReels[element[0]].length);
+    matrix[element[1]][element[0]] = null;
 
-    console.log("SYMBOL before  changing" +gameSettings.resultSymbolMatrix[element[1]][element[0]]);
-    
-    gameSettings.resultSymbolMatrix[element[1]][element[0]] =  getLastindex(element[0],(gameSettings._winData.resultReelIndex[element[0]]+element[1]));;
-    console.log( "reel Lenght" + gameSettings.tempReels[element[0]].length+ "  Changing Reel Index " + (gameSettings._winData.resultReelIndex[element[0]]+element[1]));
-    
-    removeElement(gameSettings.tempReels,element[0],gameSettings._winData.resultReelIndex[element[0]]);
-    
-    console.log("SYMBOL After changing " +gameSettings.resultSymbolMatrix[element[1]][element[0]]);
-  });
+  })
+  const movedArray = cascadeMoveTowardsNull(matrix);
+
+  let transposed = transposeMatrix(movedArray);
+  let iconsToFill:number[][]=[]
+  for (let i = 0; i < transposed.length; i++) {
+    let row=[]
+    for (let j = 0; j < transposed[i].length; j++) {
+      if (transposed[i][j] == null || undefined) {
+        let index = (gameSettings.resultReelIndex[i] + j + 2) % gameSettings.tempReels[i].length;
+        transposed[i][j] = gameSettings.tempReels[i][index];
+        row.unshift(gameSettings.tempReels[i][index]);
+      }
+
+    }
+    if(row.length>0)
+    iconsToFill.push(row);
+  }
+
+
+  matrix = transposeMatrix(transposed);
+  // matrix.pop();
+  // matrix.pop();
+  // matrix.pop();
+  // matrix.push([ '1', '2', '3', '4', '5' ])
+  // matrix.push([ '1', '1', '1', '1', '6' ])
+  // matrix.push([ '0', '0', '0', '0', '0' ])
+  console.log("iconsTofill",iconsToFill);
+  gameSettings.resultSymbolMatrix=matrix;
+
+  // index.forEach(element => {
+  //   console.log("X : " + element[0] + " Y : " + element[1]);
+  //   console.log("REEL LENGTH " + gameSettings.tempReels[element[0]].length);
+
+  //   console.log("SYMBOL before  changing" + gameSettings.resultSymbolMatrix[element[1]][element[0]]);
+
+  //   console.log(gameSettings.resultReelIndex);
+
+  //   removeElement(gameSettings.tempReels, element[0], gameSettings.resultReelIndex[element[0]]);
+  //   gameSettings.resultSymbolMatrix[element[1]][element[0]] = getLastindex(element[0], (gameSettings.resultReelIndex[element[0]] + element[1]));
+
+  //   console.log("reel Lenght" + gameSettings.tempReels[element[0]].length + "  Changing Reel Index " + (gameSettings.resultReelIndex[element[0]] + element[1]));
+
+
+  //   console.log("SYMBOL After changing " + gameSettings.resultSymbolMatrix[element[1]][element[0]]);
+  // });
 
   const result = new CheckResult();
-    result.makeResultJson(ResultType.moolah);
+  result.makeResultJson(ResultType.moolah,iconsToFill);
 }
 
 function getLastindex(reelIndex: number, index: number) {
-  if(index >= gameSettings.tempReels[reelIndex].length)
+  if (index >= gameSettings.tempReels[reelIndex].length)
+  if (index >= gameSettings.tempReels[reelIndex].length)
     index = index - gameSettings.tempReels[reelIndex].length;
-  
-  console.log(index);
+
+  console.log("index __", index);
 
 
-  let Index = index -1;
+  let Index = index - 1;
   console.log("Changed Index " + Index);
-  if (Index < 0)
-    {
-      Index = gameSettings.tempReels[reelIndex].length - 1;
-      console.log("Reel Lenght " +  gameSettings.tempReels[reelIndex].length + " Changed value below Zero " + Index);
-      return gameSettings.tempReels[reelIndex][Index]
-    }
+  if (Index < 0) {
+    Index = gameSettings.tempReels[reelIndex].length - 1;
+    console.log("Reel Lenght " + gameSettings.tempReels[reelIndex].length + " Changed value below Zero " + Index);
+    return gameSettings.tempReels[reelIndex][Index]
+  }
   else return gameSettings.tempReels[reelIndex][Index];
 }
-function removeElement(arr: string[][], rowIndex: number, colIndex: number): void {
+function removeElement(
+  arr: string[][],
+  rowIndex: number,
+  colIndex: number
+): void {
   console.log("row : " + rowIndex + " col : " + colIndex);
   console.log("temp Reel " + gameSettings.tempReels[rowIndex]);
   console.log(arr[rowIndex].length);
-  
-  
+
+
   // if (rowIndex < 0 || rowIndex >= arr.length || colIndex < 0 || colIndex >= arr[rowIndex].length) {
   //     throw new Error('Invalid indices provided '+ rowIndex + " " + colIndex);
   // }
@@ -340,13 +383,66 @@ function removeElement(arr: string[][], rowIndex: number, colIndex: number): voi
 
   // Shift elements to the left to fill the removed position
   for (let i = rowIndex; i < arr.length; i++) {
-      for (let j = colIndex; j < arr[i].length; j++) {
-          if (j + 1 < arr[i].length) {
-              arr[i][j] = arr[i][j + 1];
-          } else {
-              // If we are at the last column, remove the last element
-              arr[i].pop();
-          }
+    for (let j = colIndex; j < arr[i].length; j++) {
+      if (j + 1 < arr[i].length) {
+        arr[i][j] = arr[i][j + 1];
+      } else {
+        // If we are at the last column, remove the last element
+        arr[i].pop();
       }
+    }
+    for (let j = colIndex; j < arr[i].length; j++) {
+      if (j + 1 < arr[i].length) {
+        arr[i][j] = arr[i][j + 1];
+      } else {
+        // If we are at the last column, remove the last element
+        arr[i].pop();
+      }
+    }
   }
+}
+
+function cascadeMoveTowardsNull(arr: (string | null)[][]): (string | null)[][] {
+  // Check if the array is empty or if it has empty subarrays
+  if (arr.length === 0 || arr[0].length === 0) return arr;
+
+  // Determine the number of rows and columns
+  const numRows = arr.length;
+  const numCols = arr[0].length;
+
+  // Create a new array to store the result
+  let result: (string | null)[][] = Array.from({ length: numRows }, () => new Array(numCols).fill(null));
+
+  for (let col = 0; col < numCols; col++) {
+    let newRow = numRows - 1;
+
+    // Place non-null elements starting from the bottom
+    for (let row = numRows - 1; row >= 0; row--) {
+      if (arr[row][col] !== null) {
+        result[newRow][col] = arr[row][col];
+        newRow--;
+      }
+    }
+
+    // Fill the top positions with null if there are remaining positions
+    for (let row = newRow; row >= 0; row--) {
+      result[row][col] = null;
+    }
+  }
+
+  return result;
+}
+
+function transposeMatrix(matrix) {
+  let transposed = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    let newRow = [];
+    for (let j = 0; j < matrix.length; j++) {
+      newRow.push(matrix[j][i]);
+    }
+    transposed.push(newRow);
+  }
+
+  return transposed;
 }
