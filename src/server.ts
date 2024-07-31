00931970cb9b0e6c2ac5b43c6d1575688f4ec36a
TrippyRahul <129256734+TrippyRahul@users.noreply.github.com>
2024-07-31 12:50:11 +0530

    Update express json limit

--- src/server.ts
+++ src/server.ts
@@ -41,8 +41,8 @@ app.use(
 );
 
 //Cloudinary configs
-app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ limit: "25mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 const allowedOrigins = ['http://localhost:3000', 'https://game-rtp-backend-w7g7.onrender.com'];
 
 app.use((req, res, next) => {
