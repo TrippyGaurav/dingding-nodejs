ba7cb9f870b7831214f39f7dfb37015f664663d0
TrippyRahul <rahulrawat.trippybug@gmail.com>
2024-08-16 17:44:20 +0530

    new main base

--- src/game/Utils/globalTypes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GAMETYPE = void 0;
-var GAMETYPE;
-(function (GAMETYPE) {
-    GAMETYPE["SLOT"] = "SL";
-    GAMETYPE["KENO"] = "KN";
-})(GAMETYPE || (exports.GAMETYPE = GAMETYPE = {}));
