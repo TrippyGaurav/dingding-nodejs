8daf11bc6a1fca16d0324e6afbb759d48171ce41
TrippyRahul <rahulrawat.trippybug@gmail.com>
2024-08-29 12:05:34 +0530

    changed .env values

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,17 +7,16 @@ const _config = {
   databaseUrl: process.env.MONGOURL,
   env: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_NAME_API_KEY,
+  api_secret: process.env.CLOUDINARY_NAME_API_SECRET,
   companyApiKey: process.env.COMPANY_API_KEY,
   phonenumber: process.env.PHONENUMBER,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
-  region: process.env.AWS_REGION || '',
+  region: process.env.AWS_REGION  ||'',
   emailSource: process.env.EMAILSOURCE,
   platformName: process.env.PLATFORM_NAME,
   sentToemail: process.env.SENT_TO_EMAIL
 };
-
 export const config = Object.freeze(_config);
