2a0f91a63c1fb0616cb6a9b2c88602078f1c49b8
rohit <rohit@underpinservices.com>
2024-09-25 17:58:30 +0530

    fix: check games toggle #2

--- src/dashboard/middleware/checkToggle.ts
+++ src/dashboard/middleware/checkToggle.ts
@@ -8,16 +8,15 @@ import createHttpError from 'http-errors';
 export const checkLoginToggle = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const companyUsers = await User.find({ role: 'company' });
-    //check if company users exist ,then go through available function
-    if(companyUsers?.find(user => user.username === req.body.username)){ 
-      console.log("passed through");
+    //check if company users exist ,then pass through
+    if (companyUsers?.find(user => user.username === req.body.username)) {
       next()
-    }else{
-      const {underMaintenance, availableAt} = await isAvaiable();
-      if(underMaintenance === true) {
-        res.status(200).json({ message:`underMaintenance till ${new Date(availableAt)}` , isUnderMaintenance: underMaintenance });
+    } else {
+      const { underMaintenance, availableAt } = await isAvaiable();
+      if (underMaintenance === true) {
+        res.status(200).json({ message: `underMaintenance till ${new Date(availableAt)}`, isUnderMaintenance: underMaintenance });
         return
-      }else{
+      } else {
         next()
       }
     }
@@ -28,34 +27,41 @@ export const checkLoginToggle = async (req: Request, res: Response, next: NextFu
 
 export const checkGamesToggle = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const {underMaintenance, availableAt} = await isAvaiable();
-    if(underMaintenance === true) {
-      res.status(201).json({ message:`underMaintenance till ${new Date(availableAt)}` , isUnderMaintenance: underMaintenance });
-      return
-    }else{
+
+    const companyUsers = await User.find({ role: 'company' });
+    //check if company users exist ,then pass through
+    if (companyUsers?.find(user => user.username === req.body.username)) {
       next()
+    } else {
+      const { underMaintenance, availableAt } = await isAvaiable();
+      if (underMaintenance === true) {
+        res.status(201).json({ message: `underMaintenance till ${new Date(availableAt)}`, isUnderMaintenance: underMaintenance });
+        return
+      } else {
+        next()
+      }
     }
   } catch (error) {
     next(error);
   }
 }
 
-async function isAvaiable (){
+async function isAvaiable() {
   const toggle = await Toggle.findOne();
-  if(!toggle) throw createHttpError(404, "Toggle not found");
-  if(toggle.availableAt === null) {
-    return {underMaintenance: false, availableAt: null}
+  if (!toggle) throw createHttpError(404, "Toggle not found");
+  if (toggle.availableAt === null) {
+    return { underMaintenance: false, availableAt: null }
   }
   const now = new Date();
   const time = new Date(toggle.availableAt);
   if (time > now) {
-    return {underMaintenance: true, availableAt: toggle.availableAt}
-  }else{
+    return { underMaintenance: true, availableAt: toggle.availableAt }
+  } else {
     await Toggle.findOneAndUpdate(
       {},
       { availableAt: null },
       { new: true, upsert: true }
     )
-    return {underMaintenance: false, availableAt: null}
+    return { underMaintenance: false, availableAt: null }
   }
 }
